45400ccf16007bf4f1068abc4d9634e5d6f386f6
Maletin Nikita <faradzen@gmail.com>
2013-07-25 22:20:09 +0400

    [upd] add observer pattern

---
+++ 910_ConsoleApp_Memento/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _910_ConsoleApp_Memento
+{
+
+
+    class Item
+    {
+        class Memento
+        {
+            public string Name { get; private set; }
+            public string Code { get; private set; }
+            public Memento(string name, string code)
+            { Commit(name, code); }
+            public void Commit(string name, string code)
+            {
+                Name = name;
+                Code = code;
+            }
+        }
+
+        public string Name { get; set; }
+        public string Code { get; set; }
+        private Memento _safe;
+
+        public Item(string name, string code)
+        {
+            Name = name;
+            Code = code;
+            _safe = new Memento(name, code);
+        }
+        public void Commit() { _safe.Commit(Name, Code); }
+        public void Revert()
+        {
+            Name = _safe.Name;
+            Code = _safe.Code;
+        }
+        public void Print() { Console.WriteLine("Name:{0}, Code:{1}", Name, Code); }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MEMENTO");
+            var item = new Item("pen", "p001");
+            item.Print();
+            item.Name = "pencil";
+            item.Print();
+            item.Revert();
+            item.Print();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 911_ConsoleApp_Observer/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _911_ConsoleApp_Observer
+{
+    public interface INotify
+    {
+        void Notify();
+    }
+
+    class MailNotif : INotify
+    {
+        public void Notify()
+        {
+            Console.WriteLine("Mail notification");
+        }
+    }
+
+    class SmsNotif : INotify
+    {
+        public void Notify()
+        {
+            Console.WriteLine("Sms notification");
+        }
+    }
+
+    class EventNotif : INotify
+    {
+        public void Notify()
+        {
+            Console.WriteLine("Event notification");
+        }
+    }
+
+    class Notifier
+    {
+        ArrayList _notificators = new ArrayList();
+        public void AddNotif(INotify n)
+        {
+            _notificators.Add(n);
+        }
+        public void RemoveNotif(INotify n)
+        {
+            _notificators.Remove(n);
+        }
+        public void NotifAll()
+        {
+            foreach (INotify notificator in _notificators)
+            {
+                notificator.Notify();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Observer");
+            var n1 = new MailNotif();
+            var n2 = new SmsNotif();
+            var observer = new Notifier();
+            observer.AddNotif(n1);
+            observer.AddNotif(n2);
+            observer.NotifAll();
+            Console.WriteLine("REMOVE ONE");
+            observer.RemoveNotif(n1);
+            observer.NotifAll();
+            Console.ReadKey();
+        }
+    }
+}
